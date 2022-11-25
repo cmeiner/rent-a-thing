ce72bab59a71aaff6e307d0478dbc29b26282d74
Jannie87 <jannie.backman@gmail.com>
2022-11-25 11:40:18 +0100

    created sign up form

---
+++ pages/register/index.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from "next";
+import { Header } from "../../src/components/big/header/Header";
+import { InputField } from "../../src/components/small/inputfield/InputField";
+import { PrimaryButton } from "../../src/components/small/primarybtn/PrimaryBtn";
+import styles from "./RegisterPage.module.scss";
+
+const Register: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      <div className={styles.container}>
+        <h2 className={styles.title}>Skapa konto</h2>
+        <form className={styles.formStyle}>
+          <InputField placeholder={"Name"} type={"text"} />
+          <InputField placeholder={"Email"} type={"text"} />
+          <InputField placeholder={"Password"} type={"text"} />
+          <PrimaryButton
+            text="Skapa konto"
+            onClick={() => console.log("TODO")}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
