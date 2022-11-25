0226ef7cd9b0fbc750a51c8448a77f133125f1b4
Jannie87 <jannie.backman@gmail.com>
2022-11-25 11:55:30 +0100

    added handle Submit on form

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -5,12 +5,17 @@ import { PrimaryButton } from "../../src/components/small/primarybtn/PrimaryBtn"
 import styles from "./RegisterPage.module.scss";
 
 const Register: NextPage = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <div>
       <Header />
       <div className={styles.container}>
         <h2 className={styles.title}>Skapa konto</h2>
-        <form className={styles.formStyle}>
+        <form onSubmit={handleSubmit} className={styles.formStyle}>
           <InputField placeholder={"Name"} type={"text"} />
           <InputField placeholder={"Email"} type={"text"} />
           <InputField placeholder={"Password"} type={"text"} />
