a7a1014574dcab8bcf43fd83bf009345a9ffcdf0
Love Lanai <love.lanai@medieinstitutet.se>
2022-11-25 10:14:55 +0100

    post is now working fine, get is working, but not fully functional

--- src/utils/Hooks.ts
+++ src/utils/Hooks.ts
@@ -1,34 +1,24 @@
 import { db } from "../firebase/Firebase";
 import { collection, doc, setDoc, getDocs } from "firebase/firestore";
 
-const postsRef = collection(db, "posts");
-const usersRef = collection(db, "users");
+// const postsRef = collection(db, "posts");
+// const usersRef = collection(db, "users");
 
-interface FetchProps {
-  api: string;
-  id?: string;
-}
-
-export interface PostProps {
-  api: string;
-  data: DataProps;
-}
-
-interface DataProps {
+interface PostProps {
   picture: string;
   title: string;
   desc: string;
   price: string;
 }
 
-export const usePost = async ({ api, data }: PostProps) => {
-  await setDoc(doc(postsRef), {
+export const usePost = async (api: string, data: {}) => {
+  await setDoc(doc(collection(db, api)), {
     data,
   });
-  console.log("added data to database :)");
+  console.log(data, "added to the database");
 };
 
-export const useFetch = async ({ api, id }: FetchProps) => {
+export const useFetch = async (api: string, id?: string) => {
   const querySnapshot = await getDocs(collection(db, api));
   querySnapshot.forEach((doc) => {
     const response = doc.data();
