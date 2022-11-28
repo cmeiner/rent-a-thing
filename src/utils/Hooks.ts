d3ee4e2cd0d6bd4a8c3d98679b63598c62bf85c1
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2022-11-28 11:59:27 +0100

    Fixed fetch by id

--- src/utils/Hooks.ts
+++ src/utils/Hooks.ts
@@ -1,9 +1,9 @@
-import { collection, doc, getDocs, setDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from "../firebase/Firebase";
+import { collection, doc, getDocs, setDoc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase/Firebase';
 
-const postsRef = collection(db, "posts");
-const usersRef = collection(db, "users");
+const postsRef = collection(db, 'posts');
+const usersRef = collection(db, 'users');
 
 export interface PostProps {
   title?: string;
@@ -17,28 +17,28 @@ export const usePost = async (api: string, data: {}) => {
   await setDoc(doc(collection(db, api)), {
     data,
   });
-  console.log(data, "added to the database");
+  console.log(data, 'added to the database');
 };
-
-// export const useFetch = async (api: string, id?: string) => {
-//   const querySnapshot = await getDocs(collection(db, api));
-//   querySnapshot.forEach((doc) => {
-//     const response = doc.data();
-//     console.log(response);
-//   });
-// };
 
 export const useFetch = (api: string, id?: string) => {
   const [response, setResponse] = useState([]);
 
   useEffect(() => {
-    getDocs(collection(db, api)).then((res) => {
-      setResponse(
-        res.docs.map((item) => {
-          return { ...item.data().data };
-        }) as any
-      );
-    });
+    if (!id) {
+      getDocs(collection(db, api)).then((res) => {
+        setResponse(
+          res.docs.map((item) => {
+            return { ...item.data().data, id: item.id };
+          }) as any
+        );
+      });
+    } else {
+      const postById = doc(db, api, id);
+      getDoc(postById).then((doc) => {
+        const singlePost = [{ ...doc.data()?.data, id: doc.id }] as any;
+        setResponse(singlePost);
+      });
+    }
   }, []);
 
   return { response };
