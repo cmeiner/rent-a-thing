e17876ffe160c703a31bbfa4cbec4a85f82facad
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2022-11-28 12:05:12 +0100

    removed dummy data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,15 @@
-import { NextPage } from "next";
-import Head from "next/head";
-import { Footer } from "../src/components/big/footer/Footer";
-import { Header } from "../src/components/big/header/Header";
-import { BannerText } from "../src/components/small/bannerText/BannerText";
-import { FilterAndText } from "../src/components/small/filterAndText/FilterAndText";
-import { PrimaryButton } from "../src/components/small/primarybtn/PrimaryBtn";
-import { ProductCard } from "../src/components/small/productcard/ProductCard";
-import { PostProps, useFetch, usePost } from "../src/utils/Hooks";
-import styles from "./index.module.scss";
+import { NextPage } from 'next';
+import Head from 'next/head';
+import { Footer } from '../src/components/big/footer/Footer';
+import { Header } from '../src/components/big/header/Header';
+import { BannerText } from '../src/components/small/bannerText/BannerText';
+import { FilterAndText } from '../src/components/small/filterAndText/FilterAndText';
+import { ProductCard } from '../src/components/small/productcard/ProductCard';
+import { PostProps, useFetch } from '../src/utils/Hooks';
+import styles from './index.module.scss';
 
 const Home: NextPage = () => {
-  const { response } = useFetch("posts");
-
-  const dummyData = {
-    title: "hammare",
-    desc: "very nice hammer",
-    picture: "",
-    price: "3000",
-  };
-
-  const test = () => {
-    usePost("posts", dummyData);
-  };
+  const { response } = useFetch('posts');
 
   return (
     <div>
@@ -35,19 +23,18 @@ const Home: NextPage = () => {
       <FilterAndText />
       <div className={styles.productContainer}>
         <div className={styles.productGrid}>
-          {response?.slice(0, 10).map((posts: PostProps, key) => {
+          {response?.slice(0, 10).map((posts: PostProps, key: any) => {
             return (
               <ProductCard
                 key={key}
                 title={posts.title}
                 price={posts.price}
-                image={"https://picsum.photos/200"}
+                image={posts.img}
               />
             );
           })}
         </div>
       </div>
-      <PrimaryButton onClick={test} submit={false} text="add" />
       <Footer />
     </div>
   );
