0f579b7f505bafbf9b4c556f00d5ac5e2a8e7ddf
Love Lanai <love.lanai@medieinstitutet.se>
2022-11-28 09:50:15 +0100

    added functional useFetch for getting data from db

--- src/utils/Hooks.ts
+++ src/utils/Hooks.ts
@@ -1,14 +1,16 @@
+import { collection, doc, getDocs, setDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { db } from "../firebase/Firebase";
-import { collection, doc, setDoc, getDocs } from "firebase/firestore";
 
-// const postsRef = collection(db, "posts");
-// const usersRef = collection(db, "users");
+const postsRef = collection(db, "posts");
+const usersRef = collection(db, "users");
 
-interface PostProps {
-  picture: string;
-  title: string;
-  desc: string;
-  price: string;
+export interface PostProps {
+  title?: string;
+  desc?: string;
+  picture?: string;
+  price?: string;
+  id?: string;
 }
 
 export const usePost = async (api: string, data: {}) => {
@@ -18,10 +20,26 @@ export const usePost = async (api: string, data: {}) => {
   console.log(data, "added to the database");
 };
 
-export const useFetch = async (api: string, id?: string) => {
-  const querySnapshot = await getDocs(collection(db, api));
-  querySnapshot.forEach((doc) => {
-    const response = doc.data();
-    console.log(response);
-  });
+// export const useFetch = async (api: string, id?: string) => {
+//   const querySnapshot = await getDocs(collection(db, api));
+//   querySnapshot.forEach((doc) => {
+//     const response = doc.data();
+//     console.log(response);
+//   });
+// };
+
+export const useFetch = (api: string, id?: string) => {
+  const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    getDocs(collection(db, api)).then((res) => {
+      setResponse(
+        res.docs.map((item) => {
+          return { ...item.data().data };
+        }) as any
+      );
+    });
+  }, []);
+
+  return { response };
 };
