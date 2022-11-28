7bd347cb0dea0b0d6e2b9d4d7b1766d6b8ff7927
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2022-11-28 08:06:17 +0100

    Design startpage - icon och text for filter Fixes #34

---
+++ src/components/small/filterAndText/FilterAndText.tsx
@@ -0,0 +1,10 @@
+import TuneIcon from '@mui/icons-material/Tune';
+import styles from './FilterAndText.module.scss';
+
+export const FilterAndText = () => {
+  return (
+    <div className={styles.container}>
+      <TuneIcon /> <p>Filtrera</p>
+    </div>
+  );
+};
