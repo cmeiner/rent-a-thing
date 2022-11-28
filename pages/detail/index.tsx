28b4116e42045900e005c8f2dd88efe7db5a4149
miMeiner <christian.meiner@medieinstitutet.se>
2022-11-28 11:00:22 +0100

    Add the finished design of the details card

--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -3,29 +3,31 @@ import { Header } from "../../src/components/big/header/Header";
 import { PrimaryButton } from "../../src/components/small/primarybtn/PrimaryBtn";
 import styles from "./DetailPage.module.scss";
 
-const Products: NextPage = () => {
-  const product = {
-    title: "Partytält",
-    image: "https://picsum.photos/900/900",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta quas illum laudantium dolor amet, fugiat non cupiditate eos quam in! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta quas illum laudantium dolor amet, fugiat non cupiditate eos quam in.",
-    price: "Från 300kr per dygn",
-    category: "Utomhus",
-  };
+const Details: NextPage = () => {
+  // const product = {
+  //   title: "Partytält",
+  //   image: "https://picsum.photos/900/900",
+  //   desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta quas illum laudantium dolor amet, fugiat non cupiditate eos quam in! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta quas illum laudantium dolor amet, fugiat non cupiditate eos quam in.",
+  //   price: "Från 300kr / dygn",
+  //   category: "Utomhus",
+  // };
 
   return (
     <>
-      <div className={styles.productPage}>
       <Header />
-        <div className={styles.imageSection}>
-          <h1 className={styles.productTitle}>{product.title}</h1>
-          <img src={product.image} className={styles.productImage} />
-        </div>
-        <div className={styles.textSection}>
-          <div className={styles.descHeader}>
-            <h2 className={styles.productPrice}>{product.price}</h2>
-            <h2>{product.category}</h2>
+      <div className={styles.productPage}>
+        <div className={styles.productCard}>
+          <div className={styles.imageSection}>
+            <h1 className={styles.productTitle}>{product.title}</h1>
+            <img src={product.image} className={styles.productImage} />
           </div>
-          <h2 className={styles.productDesc}>{product.desc}</h2>
+          <div className={styles.textSection}>
+            <h2 className={styles.productDesc}>{product.desc}</h2>
+            <div className={styles.descHeader}>
+              <h2 className={styles.productPrice}>{product.price}</h2>
+              <h2>{product.category}</h2>
+            </div>
+          </div>
         </div>
         <div className={styles.buttonSection}>
           <PrimaryButton
@@ -38,4 +40,4 @@ const Products: NextPage = () => {
   );
 };
 
-export default Products;
+export default Details;
