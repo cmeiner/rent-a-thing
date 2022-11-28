15f480dc1a0b2f67fbf1328f4dd8fc99faad8bdd
Love Lanai <love.lanai@medieinstitutet.se>
2022-11-28 20:24:01 +0100

    implemented firestoreAuth for user. Added context for user-state

---
+++ src/auth/AuthContext.ts
@@ -0,0 +1,6 @@
+import React, { createContext } from "react";
+
+export const AuthContext = createContext({
+  currentUser: {},
+  setCurrentUser: (currentUser: {}) => {},
+});
--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,3 +23,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth();
