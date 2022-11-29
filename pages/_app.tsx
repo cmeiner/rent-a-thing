9ea107ca331c1e361dda7be7672ee31123765fc0
Love Lanai <love.lanai@medieinstitutet.se>
2022-11-29 10:34:52 +0100

    made a useffect for stating if user is logged in or not

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { onAuthStateChanged } from "firebase/auth";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthContext } from "../src/auth/AuthContext";
 import { auth } from "../src/firebase/Firebase";
 import "../styles/globals.css";
@@ -8,21 +8,25 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   const [currentUser, setCurrentUser] = useState({});
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-      setCurrentUser({
-        email: user.email,
-        id: uid,
-        photo: user.photoURL,
-        name: user.displayName,
-      });
-      // ...
-    }
-  });
-
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        setCurrentUser({
+          email: user.email,
+          id: uid,
+          photo: user.photoURL,
+          name: user.displayName,
+        }); // ...
+        console.log("inloggad", user);
+      } else {
+        setCurrentUser({});
+        console.log("ej inloggad", user);
+      }
+    });
+  }, []);
   return (
     <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
       <Component {...pageProps} />
