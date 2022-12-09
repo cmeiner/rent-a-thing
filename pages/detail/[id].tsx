8c6e5d382aac7b0a0cf3310dc5dff842f38d6350
Love Lanai <love.lanai@medieinstitutet.se>
2022-12-09 13:41:25 +0100

    new design detailpage

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,13 +1,15 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import { NextPage } from 'next';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import style from 'styled-jsx/style';
 import { Header } from '../../src/components/big/header/Header';
+import { Slider } from '../../src/components/big/sliderbtn/Slider';
 import { FilterDay } from '../../src/components/small/filterDay/FilterDay';
 import { PrimaryButton } from '../../src/components/small/primarybtn/PrimaryBtn';
 import {
@@ -20,10 +22,11 @@ import {
 import styles from './DetailPage.module.scss';
 
 const Details: NextPage = () => {
+  const [switchState, setSwitchState] = useState(false);
+
   const router = useRouter();
   const id = router.query.id as string;
   const { response: productRes } = useFetch('posts', id);
-
   const productData = { ...(productRes as unknown as ProductProps) };
 
   // const requestData = { ...(productRes as unknown as RequestProps) };
@@ -60,87 +63,107 @@ const Details: NextPage = () => {
     <>
       <Header />
       <div className={styles.productPage}>
-        <div className={styles.productCard}>
-          <div className={styles.aside}>
-            <div className={styles.navigationContainer}>
-              <KeyboardBackspaceIcon
-                className={styles.arrow}
-                onClick={() => router.back()}
-              />
-            </div>
-            <div className={styles.productImage}>
-              <Image
-                src={productData.img as string}
-                alt="image"
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-            <div className={styles.titleContainer}>
-              <h1 className={styles.title}>{productData.title}</h1>
-            </div>
+        <div className={styles.productContainer}>
+          <div className={styles.nav}>
+            <KeyboardBackspaceIcon
+              className={styles.icon}
+              onClick={() => router.back()}
+            />
           </div>
-          <div className={styles.textSection}>
-            <div className={styles.infoContainer}>
-              <div className={styles.infoContent}>
-                <div className={styles.info}>
-                  <div className={styles.text}>
-                    <p>Kategori:</p>
-                    <h2>{productData.category}</h2>
-                  </div>
-                  <div className={styles.text}>
-                    <p>Pris per dag:</p>
-                    <h2>{productData.price}:-</h2>
-                  </div>
-                </div>
-                <div className={styles.subInfo}>
-                  <div className={styles.test}>
+          <div className={styles.productContent}>
+            <div className={styles.imageContainer}>
+              <div className={styles.productImage}>
+                <Image
+                  src={productData.img as string}
+                  alt="image"
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+            </div>
+            <div className={styles.aside}>
+              <div className={styles.productNav}>
+                <h1>{productData.title}</h1>
+                <Slider
+                  state={switchState}
+                  onClick={() => setSwitchState(!switchState)}
+                  primary="Produktinfo"
+                  secondary="Beskrivning"
+                />
+              </div>
+              <div className={styles.textContainer}>
+                {!switchState ? (
+                  <>
                     <div className={styles.text}>
-                      <p>Beskrivning</p>
-
-                      <h2>{productData.desc}</h2>
+                      <h3 className={styles.title}>Kategori</h3>
+                      <p>{productData.category}</p>
                     </div>
-                    <div className={styles.testtwo}>
-                      <Link href={`/userprofile/${productData.postedBy}`}>
-                        <a className={styles.postedBy}>
-                          <p>Hyrs ut av: </p>
+                    <div className={styles.text}>
+                      <h3 className={styles.title}>Produken är uthyrd</h3>
+                      <h1 className={styles.data}>
+                        {productData.timesRented} gånger
+                      </h1>
+                    </div>
 
-                          <h3>
-                            {userData?.displayName}
-                            <AccountCircleIcon className={styles.icon} />
-                          </h3>
-                        </a>
+                    <div className={styles.link}>
+                      <div className={styles.text}>
+                        <h3 className={styles.title}>Hyrs ut utav:</h3>
+                        <h1 className={styles.data}>
+                          {userData.displayName}
+                          <AccountCircleIcon className={styles.icon} />
+                        </h1>
+                      </div>
+                      <Link href={`/userprofile/${productData.postedBy}`}>
+                        <p>Se fler annonser från {userData.displayName}</p>
                       </Link>
-                      <div className={styles.buttonSection}>
-                        {user.id ? (
+                    </div>
+                  </>
+                ) : (
+                  <div className={styles.desc}>
+                    <h3>Beskrivning</h3>
+                    <p>
+                      {productData.desc.length
+                        ? productData.desc
+                        : 'Produkten saknar beskrivning...'}
+                    </p>
+                  </div>
+                )}
+                <hr />
+                <div className={styles.submit}>
+                  {user.id ? (
+                    <>
+                      <div className={styles.formContainer}>
+                        {productData.available ? (
                           <>
-                            {!productData.available ? (
-                              <div className={styles.loginUser}>
-                                <p className={styles.infoText}>
-                                  Produkten är uthyrd för tillfället.
-                                </p>
-                              </div>
-                            ) : (
-                              <form onSubmit={HandleSubmit}>
-                                <FilterDay onChange={HandleOption} />
-                                <PrimaryButton text="Rent-this-thing" submit />
-                                <ToastContainer />
-                              </form>
-                            )}
+                            <p>Vill du hyra denna produkt?</p>
+                            <div className={styles.text}>
+                              <h3 className={styles.title}>Pris per dag:</h3>
+                              <h1 className={styles.data}>
+                                {productData.price} :-
+                              </h1>
+                            </div>
+                            <form onSubmit={HandleSubmit}>
+                              <FilterDay onChange={HandleOption} />
+                              <PrimaryButton text="Rent-this-thing" submit />
+                            </form>
                           </>
                         ) : (
-                          <div className={styles.loginUser}>
-                            <p className={styles.infoText}>
-                              Vänligen logga in för att hyra denna produkt
-                            </p>
-                            <Link href={'/login'}>
-                              <p className={styles.link}>Logga in</p>
-                            </Link>
-                          </div>
+                          <p className={styles.infoText}>
+                            Produkten är uthyrd för tillfället.
+                          </p>
                         )}
                       </div>
+                    </>
+                  ) : (
+                    <div className={styles.loginUser}>
+                      <p className={styles.infoText}>
+                        Vänligen logga in för att hyra denna produkt
+                      </p>
+                      <Link href={'/login'}>
+                        <p className={styles.link}>Logga in</p>
+                      </Link>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
             </div>
