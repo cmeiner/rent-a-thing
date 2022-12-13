90f2b3229a993afabd4b7246bd6ccb6a0e81584d
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2022-12-13 10:04:14 +0100

    added cy. to connect test

--- cypress/e2e/AddDescription.cy.ts
+++ cypress/e2e/AddDescription.cy.ts
@@ -1,30 +1,22 @@
 describe('AddDescription', () => {
   it('Adds description on profilepage', () => {
-    cy.visit('localhost:3000')
-      .get('.Header_icon__MUqiq')
-      .click()
-      .get('#toRegister')
-      .click()
-      .wait(500)
-      .get('#displayName')
-      .type('Jannie')
-      .get('#email')
-      .type(`test${Math.floor(Math.random() * 2000)}@test.com`)
-      .get('#password')
-      .type('123456')
-      .get('#profileImage')
-      .selectFile('cypress/fixtures/cat.jpg')
-      .wait(1000)
-      .get('#registerForm')
-      .submit()
-      .get('#description')
-      .wait(1000)
-      .type('A nice description')
-      .get('#descriptionForm')
-      .wait(1000)
-      .submit()
-      .get('#logOutButton')
-      .click();
+    cy.visit('localhost:3000');
+    cy.get('.Header_icon__MUqiq').click();
+    cy.get('#toRegister').click();
+    cy.wait(500);
+    cy.get('#displayName').type('Jannie');
+    cy.get('#email').type(`test${Math.floor(Math.random() * 2000)}@test.com`);
+    cy.get('#password').type('123456');
+    cy.get('#profileImage').selectFile('cypress/fixtures/cat.jpg');
+    cy.wait(1000);
+    cy.get('#registerForm').submit();
+    cy.get('#description');
+    cy.wait(1000);
+    cy.type('A nice description');
+    cy.get('#descriptionForm');
+    cy.wait(1000);
+    cy.submit();
+    cy.get('#logOutButton').click();
   });
 });
 
