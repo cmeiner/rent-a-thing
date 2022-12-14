a71bfa66064f0ecc314f2d3ef617ae2657ee77f6
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2022-12-14 15:25:04 +0100

    changed to id

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,7 @@ const Home: NextPage = () => {
         <div className={styles.navigationLink}>
           <Link href="/new">
             <div>
-              <AddCircleIcon className={styles.AddbtnLarge} />
+              <AddCircleIcon id={styles.AddbtnLarge} />
             </div>
           </Link>
         </div>
